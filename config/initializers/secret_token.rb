32584236ab82392c5354f37d1d5e74c4ce2a742f
Nell Shamrell <nshamrell@bluebox.net>
2012-01-16 19:18:21 -0800

    all files

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ThingsIBought::Application.config.secret_token = '0cb59475f8e6ac4e0aa356a01ce18d38e5e3eaed292470377fc9e7e2f4ca162ecc1bfa7d9528a687c92df0108dab6300a2785d47ba960105ff08298dc4d089b4'
